8a82b1cc4aa9071e9329eda159ecb174750a7146
atled <luca.ricci.dox@gmail.com>
2024-09-28 13:26:26 +0200

    removed unused wrapper

--- modules/external/include/gfx.hpp
+++ modules/external/include/gfx.hpp
@@ -37,36 +37,6 @@ typedef ::Color Color;  // Using the fully qualified type from raylib
 #define GFX_MAGENTA    { 255, 0, 255, 255 }     // Magenta
 #define GFX_RAYWHITE   { 245, 245, 245, 255 }   // My own White
 
-// Window-related functions
-void InitWindow(int width, int height, const char *title);  // Initialize window and OpenGL context
-void CloseWindow(void);                                     // Close window and unload OpenGL context
-bool WindowShouldClose(void);                               // Check if application should close (KEY_ESCAPE pressed or windows close icon clicked)
-bool IsWindowReady(void);                                   // Check if window has been initialized successfully
-bool IsWindowFullscreen(void);                              // Check if window is currently fullscreen
-bool IsWindowHidden(void);                                  // Check if window is currently hidden (only PLATFORM_DESKTOP)
-bool IsWindowMinimized(void);                               // Check if window is currently minimized (only PLATFORM_DESKTOP)
-bool IsWindowMaximized(void);                               // Check if window is currently maximized (only PLATFORM_DESKTOP)
-bool IsWindowFocused(void);                                 // Check if window is currently focused (only PLATFORM_DESKTOP)
-bool IsWindowResized(void);                                 // Check if window has been resized last frame
-bool IsWindowState(unsigned int flag);                      // Check if one specific window flag is enabled
-void SetWindowState(unsigned int flags);                    // Set window configuration state using flags (only PLATFORM_DESKTOP)
-void ClearWindowState(unsigned int flags);                  // Clear window configuration state flags
-void ToggleFullscreen(void);                                // Toggle window state: fullscreen/windowed [resizes monitor to match window resolution] (only PLATFORM_DESKTOP)
-void ToggleBorderlessWindowed(void);                        // Toggle window state: borderless windowed [resizes window to match monitor resolution] (only PLATFORM_DESKTOP)
-void MaximizeWindow(void);                                  // Set window state: maximized, if resizable (only PLATFORM_DESKTOP)
-void MinimizeWindow(void);                                  // Set window state: minimized, if resizable (only PLATFORM_DESKTOP)
-void RestoreWindow(void);                                   // Set window state: not minimized/maximized (only PLATFORM_DESKTOP)
-void SetWindowIcon(Image image);                            // Set icon for window (single image, RGBA 32bit, only PLATFORM_DESKTOP)
-void SetWindowIcons(Image *images, int count);              // Set icon for window (multiple images, RGBA 32bit, only PLATFORM_DESKTOP)
-void SetWindowTitle(const char *title);                     // Set title for window (only PLATFORM_DESKTOP and PLATFORM_WEB)
-void SetWindowPosition(int x, int y);                       // Set window position on screen (only PLATFORM_DESKTOP)
-void SetWindowMonitor(int monitor);                         // Set monitor for the current window
-void SetWindowMinSize(int width, int height);               // Set window minimum dimensions (for FLAG_WINDOW_RESIZABLE)
-void SetWindowMaxSize(int width, int height);               // Set window maximum dimensions (for FLAG_WINDOW_RESIZABLE)
-void SetWindowSize(int width, int height);                  // Set window dimensions
-void SetWindowOpacity(float opacity);                       // Set window opacity [0.0f..1.0f] (only PLATFORM_DESKTOP)
-void SetWindowFocused(void);                                // Set window focused (only PLATFORM_DESKTOP)
-void *GetWindowHandle(void);                                // Get native window handle
 int GetScreenWidth(void);                                   // Get current screen width
 int GetScreenHeight(void);                                  // Get current screen height
 int GetRenderWidth(void);                                   // Get current render width (it considers HiDPI)
