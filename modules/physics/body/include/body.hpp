8681af4371057eb5249ec39b85ead05b707d485f
lucaricciatl <luca.ricci.dox@gmail.com>
2024-12-14 01:57:54 +0100

    add body

---
+++ modules/physics/body/include/body.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include "Frame.hpp"
+#include "Vector3.hpp"
+
+namespace physics {
+
+class Body {
+public:
+    Body();
+    ~Body();
+};
+
+} // namespace physics
